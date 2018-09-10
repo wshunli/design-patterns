31e5e5193bef86530737cb0ceb5e75e124df2f27
wshunli <wshunli@qq.com>
2018-09-10 18:39:43 +0800

    update:桥接模式

---
+++ src/ch12/BridgePatternMain.java
@@ -0,0 +1,12 @@
+package ch12;
+
+public class BridgePatternMain {
+    public static void main(String[] args) {
+        Shape redCircle = new Circle(new RedCircle(), 100, 100, 10);
+        Shape greenCircle = new Circle(new GreenCircle(), 100, 100, 10);
+
+        redCircle.draw();
+        greenCircle.draw();
+
+    }
+}
---
+++ src/ch12/DrawAPI.java
@@ -0,0 +1,5 @@
+package ch12;
+
+public interface DrawAPI {
+    void drawCircle(int radius, int x, int y);
+}
